62ff7753014126a33727189515b101b8690b4c73
ishioka-1129 <assassin-aniki@docomo.ne.jp>
2018-05-30 17:45:31 +0900

    5/30

--- TestDB/src/Test.java
+++ TestDB/src/Test.java
@@ -2,11 +2,12 @@
 public class Test {
 	public static void main(String[] args){
 		TestUserDAO dao = new TestUserDAO();
-		dao.select("taro","123");
-		dao.selectAll();
-		dao.selectByName("taro");
-		dao.selectByPassword("123");
-		dao.updateUserNameByUserName("saburo","taro");
+//		dao.select("taro","123456789");
+//		dao.selectAll();
+//		dao.selectByName("taro");
+//		dao.selectByPassword("123");
+//		dao.updateUserNameByUserName("saburo","taro");
+//		dao.insert(4,"shiro","012");
 		dao.delete("shiro");
 	}
 
